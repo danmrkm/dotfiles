74e8c30a468ff15a8726d65a0652a8d2dcad5f64
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-06-04 02:39:04 +0900

    test2 commit

---
+++ scripts/set_github_config.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# GPG key
+UNAME='danmrkm'
+EMAIL='38092962+danmrkm@users.noreply.github.com'
+GPGKEY='AF6A9DFED164765C'
+EDITOR='emacs'
+
+#Check gpg
+if [ ! -e /usr/local/bin/gpg ]
+then
+    echo 'GPG not installed. Run "brew install gpg2"'
+    exit
+fi
+
+#Check gpg-agent pinentry-mac
+if [ ! -e /usr/local/bin/gpg-agent ] || [ ! -e /usr/local/bin/prientry-mac ]
+then
+    echo 'gpg-agent or prientry-mac not installed. Run "brew install gpg-agent prientry-mac"'
+    exit
+fi
+
+# Check gpg-agent.conf
+if [ ! -e ${HOME}/.gnupg/gpg-agent.conf ]
+then
+    echo 'gpg-agent.conf not found. Create conf file.'
+    echo 'ex) '
+    echo 'use-standard-socket'
+    echo 'pinentry-program /usr/local/bin/pinentry-mac'
+fi
+
+echo 'This script gives git config for GitHub account.'
+
+echo 'Show current local config list.'
+git config --local -l
+echo '======================================'
+echo 'Show current global config list.'
+git config --global -l
+echo '======================================'
+
+echo 'Set local git config'
+git config --local user.name ${UNAME}
+git config --local user.email ${EMAIL}
+git config --local core.editor ${EDITOR}
+git config --local gpg.program /usr/local/bin/gpg
+git config --local user.signingkey ${GPGKEY}
+git config --local commit.gpgsign true
+
+echo 'Show result local config list.'
+git config --local -l
+
+
