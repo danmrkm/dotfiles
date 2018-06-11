733261e2c651d1e4b2e88a950532f6fe8da0616b
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-06-11 21:51:42 +0900

    Modify set gitconfig and .gitignore

--- scripts/set_github_config.sh
+++ scripts/set_github_config.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# GPG key
+# GitHub settings
 UNAME='danmrkm'
 EMAIL='38092962+danmrkm@users.noreply.github.com'
-GPGKEY='99AE6990CDCCD5FA'
 EDITOR='emacs'
 
 #Check gpg
@@ -30,24 +29,27 @@ then
     exit
 fi
 
-echo 'This script gives git config for GitHub account.'
+# Get GPG signature
+GPGSIG=`LANG=C gpg --list-signatures |awk 'length($2) == 16 {print $2}'|head -1`
 
-echo 'Show current local config list.'
+echo '### This script gives git config for GitHub account. ###'
+
+echo '### Show current local config list. ###'
 git config --local -l
 echo '======================================'
-echo 'Show current global config list.'
+echo '### Show current global config list. ###'
 git config --global -l
 echo '======================================'
 
-echo 'Set local git config'
+echo '### Set local git config. ###'
 git config --local user.name ${UNAME}
 git config --local user.email ${EMAIL}
 git config --local core.editor ${EDITOR}
 git config --local gpg.program /usr/local/bin/gpg
-git config --local user.signingkey ${GPGKEY}
+git config --local user.signingkey ${GPGSIG}
 git config --local commit.gpgsign true
 
-echo 'Show result local config list.'
+echo '### Show result local config list. ###'
 git config --local -l
 
 
