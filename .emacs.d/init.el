a63d2671001d82c077ef5fc0e892f1d842913a1d
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-08-09 14:58:17 +0900

    modify init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -176,6 +176,19 @@
 
       ;; Mac でファイルを開いたときに、新たなフレームを作らない
       (setq ns-pop-up-frames nil)
+
+      ;; C-yでmacOSのクリップボードの内容を貼り付け
+      (defun copy-from-osx ()
+	(shell-command-to-string "pbpaste"))
+      (setq interprogram-paste-function 'copy-from-osx)
+      
+      ;; EmacsのバッファをmacOSのクリップボードにコピー
+      (defun paste-to-osx (text &optional push)
+	(let ((process-connection-type nil))
+	  (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+	    (process-send-string proc text)
+	    (process-send-eof proc))))
+      (setq interprogram-cut-function 'paste-to-osx)
       
       )
     ;; ウィンドウ左に列数を表示
