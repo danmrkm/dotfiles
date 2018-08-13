a2f02b9eca9786602aee5a4506333509c615fae3
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-08-13 20:29:28 +0900

    Modify openssl_crypt_shell.sh

--- scripts/openssl_crypt_shell.sh
+++ scripts/openssl_crypt_shell.sh
@@ -2,8 +2,7 @@
 
 # このシェルスクリプトの使い方
 show_usage() {
-
-    echo "Usage: $0 [-e] [-d] [-in input_file] [-out output_file] [-key key_file]" 1>&2
+    echo "Usage: $0 [-e] [-d] [-i input_file] [-o output_file] [-k key_file]" 1>&2
     echo "How to create keyfiles ..."
     echo "openssl req -x509 -nodes -newkey rsa:2048 -keyout private_ssl_key.pem -out public_key.pem -subj '/'"
     exit 1
@@ -12,37 +11,48 @@ show_usage() {
 # オプション解析
 # 4つのオプションが必須
 NUMOPT=0
-while getopts edin:out:key:h OPT
+MODE=""
+
+while getopts edi:o:k:h OPT
 do
-    case $OPT in
+    case ${OPT} in
+	# 暗号化モード
         e)
-	    if [ ${MODE} = "decrypt" ]
+	    if [ "${MODE}" = "decrypt" ]
 	    then
-		echo 'Already select mode.'
+		echo 'Already specified [-d] option.'
 		show_usage
 	    fi
 	    MODE="encrypt"
             ;;
+	# 復号化モード
         d)
-	    if [ ${MODE} = "encrypt" ]
+	    if [ "${MODE}" = "encrypt" ]
 	    then
-		echo 'Already select mode.'
+		echo 'Already specified [-e] option.'
 		show_usage
 	    fi
 	    MODE="decrypt"
             ;;
-	in)
-	    INPUT_FILE="$OPTARG"
+	# 暗号 or 復号元のファイルパス
+	i)
+	    INPUT_FILE=${OPTARG}
+	    echo ${OPTARG}
 	    ;;
-	out)
+	# 出力ファイルパス
+	o)
 	    OUTPUT_FILE="$OPTARG"
 	    ;;
-        key)
+	
+	# 暗号 or 復号に用いる鍵ファイルパス
+        k)
 	    KEY_FILE="$OPTARG"
 	    ;;
+	# ヘルプ
 	h)
 	    show_usage
             ;;
+	# その他
         \?)
 	    show_usage
             ;;
@@ -54,7 +64,7 @@ done
 shift $((OPTIND - 1))
 
 # オプション指定チェック
-if [ ! ${NUMOPT} -eq 4 ]
+if [ ! ${NUMOPT} -gt 3 ]
 then
     echo 'too few options..'
     show_usage
@@ -68,7 +78,7 @@ then
 
 elif [ -e ${OUTPUT_FILE} ]
 then
-    echo 'Already exist ' ${OUTPUT_FILE}
+    echo 'Already exist' ${OUTPUT_FILE}
     echo 'Do you delete' ${OUTPUT_FILE} "? [y/n]"
     read yn
     if [ ! ${yn} = 'y' ]
@@ -86,11 +96,11 @@ fi
 # 暗号化モード
 if [ ${MODE} = "encrypt" ]
 then
-    echo "Encrypt " ${INPUT_FILE} "..."
+    echo "Encrypt" ${INPUT_FILE} "..."
     openssl smime -encrypt -aes256 -in ${INPUT_FILE} -binary -outform DEM -out ${OUTPUT_FILE} ${KEY_FILE}
     if [ $? -eq 0 ]
     then
-	echo 'Encrypting process is succeed. Output file is ' ${OUTPUT_FILE}
+	echo 'Encrypting process is succeed. Output file is' ${OUTPUT_FILE}
 	exit 0
     fi
 fi
@@ -98,12 +108,12 @@ fi
 # 復号化モード
 if [ ${MODE} = "decrypt" ]
 then
-    echo "Decrypt " ${INPUT_FILE} "..."
+    echo "Decrypt" ${INPUT_FILE} "..."
     openssl smime -decrypt -in ${INPUT_FILE} -binary -inform DEM -inkey ${KEY_FILE} -out ${OUTPUT_FILE}
 
     if [ $? -eq 0 ]
     then
-	echo 'Decrypting process is succeed. Output file is ' ${OUTPUT_FILE}
+	echo 'Decrypting process is succeed. Output file is' ${OUTPUT_FILE}
 	exit 0
     fi
 
