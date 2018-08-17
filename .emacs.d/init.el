0c6773c1c384b8312fe6c9dabd929aa6b0ee2a45
danmrkm <38092962+danmrkm@users.noreply.github.com>
2018-08-17 12:03:46 +0900

    Modify init.el and add snippets.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,14 +14,14 @@
 
   ;; GC発動のメモリ閾値を80MBに変更
   (setq gc-cons-threshold (* gc-cons-threshold 100))
-  
+
   ;; package.elを指定
   (require 'package)
 
   ;;melpa stableを追加
    (add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
   ;; (add-to-list 'package-archives (cons "melpa-stable" "https://stable.melpa.org/packages/") t)
-  
+
   ;;パッケージを初期化
   (package-initialize)
 
@@ -33,19 +33,19 @@
 
 		    ;; csv mode
 		    csv-mode
-		    
+
 		    ;; buffer utils
 		    popwin elscreen
 
 		    ;; flycheck
 		    flycheck
-		    
+
 		    ;; python
 		    jedi py-autopep8
 
 		    ;; php
 		    php-mode ac-php
-		    
+
 		    ;; helm
 		    helm helm-gtags
 
@@ -54,14 +54,20 @@
 
 		    ;; web
 		    web-mode
-		    
+
 
 		    ;; multi-term
 		    multi-term
 
 		    ;; madhat2r
 		    madhat2r-theme
-		    
+
+		    ;; markdown-mode
+		    markdown-mode
+
+		    ;; yasnippet
+		    yasnippet
+
 		    ))
 
   (defvar install-package-list-ver25
@@ -69,11 +75,11 @@
       ;; git (ver25以上のみインストール)
       magit git-gutter
 	    ))
-      
+
 
   ;; パッケージ情報更新フラグ設定
   (defvar package-refresh-flag t)
-  
+
   ;; インストールされていないパッケージをインストールする
   (dolist (package install-package-list)
 
@@ -85,7 +91,7 @@
 	)
 
       (package-install package)))
-  
+
   ;; インストールされていないパッケージをインストールする(バージョン25以上)
   (when (> emacs-major-version 25)
     (dolist (package install-package-list-ver25)
@@ -128,14 +134,17 @@
 ;; "C-t" でウィンドウを切り替える。
 (define-key global-map (kbd "C-z") 'other-window)
 
-;; "M-n"で5行下に移動する
+;; "M-n" で5行下に移動する
 (define-key global-map (kbd "M-n") (kbd "C-u 5 C-n"))
 
-;; "M-p"で5行上に移動する
+;; "M-p" で5行上に移動する
 (define-key global-map (kbd "M-p") (kbd "C-u 5 C-p"))
 
-;; "C-c rで全置換"
+;; "C-c r" で全置換
 (define-key global-map (kbd "C-c r") 'replace-string)
+
+;; "C-c w" で whitespace-mode
+(define-key global-map (kbd "C-c w") 'whitespace-mode)
 
 ;; paren-mode：対応する括弧を強調して表示する
 ;; 表示までの秒数。初期値は0.125
@@ -155,7 +164,7 @@
 (defvar eshell-command-aliases-list
       (append
        (list
-        (list "emacs" "find-file $1")
+	(list "emacs" "find-file $1")
 	)
        )
       )
@@ -165,7 +174,7 @@
 
   ;; GUI Emacs ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   (when (eq window-system 'ns)
-    
+
     ;; Macのみ有効
     (when (eq system-type 'darwin)
 
@@ -181,7 +190,7 @@
       (defun copy-from-osx ()
 	(shell-command-to-string "pbpaste"))
       (setq interprogram-paste-function 'copy-from-osx)
-      
+
       ;; EmacsのバッファをmacOSのクリップボードにコピー
       (defun paste-to-osx (text &optional push)
 	(let ((process-connection-type nil))
@@ -189,7 +198,7 @@
 	    (process-send-string proc text)
 	    (process-send-eof proc))))
       (setq interprogram-cut-function 'paste-to-osx)
-      
+
       )
     ;; ウィンドウ左に列数を表示
     (global-linum-mode t)
@@ -198,7 +207,7 @@
     (load-theme 'madhat2r t)
     ;; (load-theme 'manoj-dark t)
     ;; (load-theme 'dark-laptop t)
-    
+
     ;; タイトルバーにファイルのフルパスを表示
     (setq frame-title-format "%f")
     (tool-bar-mode -1)
@@ -208,19 +217,19 @@
 
     ;;フレームの位置を指定
     ;; (setq initial-frame-alist
-    ;; 	(append (list '(width . 171)
-    ;; 		      '(height . 53)
-    ;; 		      '(top . 5)
-    ;; 		      '(left . 46)
-    ;; 		      )
-    ;; 		default-frame-alist))
+    ;;		(append (list '(width . 171)
+    ;;			      '(height . 53)
+    ;;			      '(top . 5)
+    ;;			      '(left . 46)
+    ;;			      )
+    ;;			default-frame-alist))
 
     ;; Paren-mode
     ;; フェイスを変更する
     (set-face-attribute 'show-paren-match-face nil
 			:background nil :foreground nil
 			:underline "#a9a9a9")
-    
+
     ;; ediff
     ;; ediffコントロールパネルを別フレームにしない
     (setq ediff-window-setup-function 'ediff-setup-windows-plain)
@@ -239,13 +248,13 @@
 
     ;; Emacs終了時に本当に終了してよいか確認する
     (setq confirm-kill-emacs 'y-or-n-p)
-    
+
     )
-  
+
   ;; php-mode ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   (when (package-installed-p 'php-mode)
-    
+
 
     (autoload 'php-mode "php-mode" t nil)
 
@@ -254,7 +263,7 @@
       '(setq php-site-url "https://secure.php.net/")
       '(setq php-manual-url 'ja)
       )
-    
+
     (add-hook 'php-mode-hook '(lambda ()
 				(auto-complete-mode t)
 				(require 'ac-php)
@@ -334,15 +343,15 @@
     )
 
   ;; quickrun ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
+
   (when (package-installed-p 'quickrun)
     ;; "C-x q"でquickrunを起動
     (define-key global-map (kbd "C-x q") 'quickrun)
     )
-  
+
   ;; Helm +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   (when (package-installed-p 'helm)
-    
+
     (require 'helm-config)
     (helm-mode 1)
 
@@ -353,17 +362,17 @@
     (global-set-key (kbd "C-x C-f") 'helm-find-files)
     ;; C-x eをhelm-recentfに割り当て
     (global-set-key (kbd "C-x e") 'helm-recentf)
-    
+
     ;; タブとhel-execute-persistent-actionを入れ替える
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
     )
 
   ;; multi-term ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  
+
   (when (package-installed-p 'multi-term)
     ;; ログインシェルをbashに設定
     ;; (setq multi-term-program "/bin/bash")
-    
+
     ;; C-x tでmulti-termを起動
     (define-key global-map (kbd "C-x t") 'multi-term)
     (define-key global-map (kbd "C-x p") 'multi-term-prev)
@@ -392,9 +401,9 @@
        )
      )
 
-    
+
     )
-  
+
   ;; gtags +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   (when (package-installed-p 'helm-gtags)
     ;; gtagsを利用する場合は事前に下記のコマンドでgtagsをインストールしておくこと
@@ -413,6 +422,49 @@
     (global-set-key (kbd "M-s") 'helm-gtags-find-symbol)
     (global-set-key (kbd "C-t") 'helm-gtags-pop-stack))
 
+
+  ;; whitespace-mode +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  (setq whitespace-style '(face           ; faceで可視化
+			   trailing       ; 行末
+			   tabs           ; タブ
+			   spaces         ; スペース
+			   empty          ; 先頭/末尾の空行
+			   space-x`mark     ; 表示のマッピング
+			   tab-mark
+			   ))
+  (setq whitespace-display-mappings
+	'((space-mark ?\u3000 [?\u25a1])
+	  (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])
+	  ))
+  ;; スペースは全角のみを可視化
+  ;; (setq whitespace-space-regexp "\\(\u3000+\\)")
+  (eval-after-load "whitespace"
+    '(progn
+  (set-face-attribute 'whitespace-trailing nil
+		      :background "Yellow50"
+		      :underline nil)
+  (set-face-attribute 'whitespace-tab nil
+		      :background "gray30"
+		      :underline nil)
+  (set-face-attribute 'whitespace-space nil
+		      :background "gray20"
+		      :foreground "white20"
+		      :underline nil)
+  ))
+  
+  ;; yasnippet +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  ;; スニペット名をidoで選択する
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  ;; snippetの格納場所を指定
+  (setq yas-snippet-dirs
+	'("~/.emacs.d/snippets"
+	  "~/.emacs.d/my_snippets" ))
+
+  ;; recentf +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  (setq recentf-auto-cleanup 'never)
+  (setq recentf-save-file "~/.emacs.d/.recentf")
+  (setq recentf-auto-save-timer (run-with-idle-timer 40 t 'recentf-save-file))
+  
   )
 
 ;;; init.el ends here
