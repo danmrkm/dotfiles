759d9a35129dc7e757ca5b85144d3531de154f41
danmrkm <38092962+danmrkm@users.noreply.github.com>
2019-02-10 18:50:54 +0900

    modify show_wifi

--- scripts/show_wifi_ssid.sh
+++ scripts/show_wifi_ssid.sh
@@ -4,7 +4,7 @@
 wifidev=`networksetup -listallhardwareports |grep -A 1 Wi-Fi|awk  'NR==2 {print $2}'`
 
 # SSID を取得
-result=`/usr/sbin/networksetup -getairportnetwork ${wifidev}|grep Wi-Fi`
+result=`/usr/sbin/networksetup -getairportnetwork ${wifidev}`
 off_flag=`echo ${result} | grep off |wc -l`
 
 # off_flag が 1 の場合、WiFi は OFF
@@ -12,5 +12,12 @@ if [ ${off_flag} -ne 0 ]
 then
     echo 'Wi-Fi off'
 else
-    echo ${result} | awk '{print $4}'
+    ssid=`echo ${result} | awk '{print $4}'`
+    if [ ${ssid} == 'associated' ]
+    then
+	echo 'Not associated'
+    else
+	echo ${ssid}
+    fi
+    
 fi
